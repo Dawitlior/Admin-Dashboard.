aae6d38247e0b72e4bd7cda2f6dd66b16f788870
Dawitlior <dawitlior777@gmail.com>
2023-02-14 18:12:14 +0200

    adding client controller

---
+++ server/controllers/client.js
@@ -0,0 +1,23 @@
+import Product from "../models/Product.js";
+import ProductStat from "../models/ProductStat.js";
+
+export const getProducts = async (request, response) => {
+  try {
+    const products = await Product.find();
+
+    const productWithStat = await Promise.all(
+      products.map(async (product) => {
+        const stat = await ProductStat.find({
+          productId: product._id,
+        });
+        return {
+          ...product._doc,
+          stat,
+        };
+      })
+    );
+    response.status(200).json(productWithStat);
+  } catch (error) {
+    response.status(404).json({ message: error.message });
+  }
+};
