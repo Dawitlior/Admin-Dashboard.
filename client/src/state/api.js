35fd48927a8db7947243893977dd6296817f9638
Dawitlior <dawitlior777@gmail.com>
2023-02-14 19:21:56 +0200

    adding Customers page client and server

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -12,8 +12,12 @@ export const api = createApi({
     getProducts: build.query({
       query: () => "client/products",
       providesTags: ["Products"]
+    }),
+    getCustomers: build.query({
+      query: () => "client/customers",
+      providesTags: ["Customers"],
     })
   }),
 });
 
-export const { useGetUserQuery ,useGetProductsQuery } = api;
+export const { useGetUserQuery ,useGetProductsQuery, useGetCustomersQuery } = api;
--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,5 +1,6 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js";
 
 export const getProducts = async (request, response) => {
   try {
@@ -17,6 +18,15 @@ export const getProducts = async (request, response) => {
       })
     );
     response.status(200).json(productWithStat);
+  } catch (error) {
+    response.status(404).json({ message: error.message });
+  }
+};
+
+export const getCustomers = async (request, response) => {
+  try {
+    const customers = await User.find({ role: "user" }).select("-password");
+    response.status(200).json(customers);
   } catch (error) {
     response.status(404).json({ message: error.message });
   }
