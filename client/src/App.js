0239a7e926010bc3d56f28b41192347eea46ecba
Dawitlior <dawitlior777@gmail.com>
2023-02-20 18:34:06 +0200

    daily component

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import Customers from "scenes/customers";
 import Transactions from "scenes/transactions";
 import Geography from "scenes/geography";
 import Overview from 'scenes/overview';
+import Daily from "scenes/daily";
 
 const App = () => {
   const mode = useSelector((state) => state.global.mode);
@@ -30,6 +31,7 @@ const App = () => {
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
+              <Route path="/daily" element={<Daily />} />
               
             </Route>
           </Routes>
