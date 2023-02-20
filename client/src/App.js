5fff85a0e66687a8b6e5176005fcffeb46359ae9
Dawitlior <dawitlior777@gmail.com>
2023-02-20 18:44:25 +0200

    monthly component

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,9 @@ import Products from "scenes/products";
 import Customers from "scenes/customers";
 import Transactions from "scenes/transactions";
 import Geography from "scenes/geography";
-import Overview from 'scenes/overview';
+import Overview from "scenes/overview";
 import Daily from "scenes/daily";
+import Monthly from "scenes/monthly";
 
 const App = () => {
   const mode = useSelector((state) => state.global.mode);
@@ -32,7 +33,7 @@ const App = () => {
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
-              
+              <Route path="/monthly" element={<Monthly />} />
             </Route>
           </Routes>
         </ThemeProvider>
