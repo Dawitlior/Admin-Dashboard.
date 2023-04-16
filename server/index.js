2c98282694a0fb81d9d621b1a2e7fcfc434827d7
Dawitlior <dawitlior777@gmail.com>
2023-04-16 21:19:11 +0300

    ENV

--- server/index.js
+++ server/index.js
@@ -31,13 +31,10 @@ app.use("/sales", salesRoutes);
 const PORT = process.env.PORT || 9000;
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(
-    "mongodb+srv://lior123:lior123@cluster0.tlt391k.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
   })
